36d5fec9c9e78087043869c41c9d530f8a4d2dcc
Jason Truluck <jason.truluck@gmail.com>
2012-12-12 23:07:37 -0500

    added rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  if ENV['RAILS_ENV'] == 'test'
+    require 'simplecov'
+    SimpleCov.start 'rails'
+    class SimpleCov::Formatter::QualityFormatter
+      def format(result)
+        SimpleCov::Formatter::HTMLFormatter.new.format(result)
+        File.open("coverage/covered_percent", "w") do |f|
+          f.puts result.source_files.covered_percent.to_f
+        end
+      end
+    end
+    SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
+  end
+
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'database_cleaner'
+  require 'capybara/rspec'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.use_transactional_examples = false
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+    config.include FactoryGirl::Syntax::Methods
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+  end
+end
+
+Spork.each_run do
+
+end
+
+
