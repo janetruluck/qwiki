0db9f9003c4758ccec76529752c0fbb889c930db
Jason Truluck <jason.truluck@gmail.com>
2012-12-12 23:09:28 -0500

    removed caching of pygments for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
   class HTMLwithPygments < Redcarpet::Render::HTML
     def block_code(code, language)
-      sha = Digest::SHA1.hexdigest(code)
-      Rails.cache.fetch ["code", language, sha].join('-') do
-        Pygments.highlight(code, lexer: language)
-      end
+      Pygments.highlight(code, lexer: language)
     end
   end
 
