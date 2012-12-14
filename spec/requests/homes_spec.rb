644aafdf673b72350c7cd696cf4f4d57f5e98fa4
Jason Truluck <jason.truluck@gmail.com>
2012-12-13 22:42:05 -0500

    updated specs

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,13 +1,62 @@
 require 'spec_helper'
 describe "Home" do
   describe "index" do
-    it "displays the home index with information about qwiki" do
-      visit root_path
+    context "given a user has logged in" do
+      let!(:user) { create(:user) }
 
-      page.should have_content("Welcome!")
-      page.should have_content("This is Qwiki, the qwik wiki.")
+      it "displays the home index with information about qwiki" do
+        login_as(user, :scope => :user)
 
-      current_path.should == root_path
+        visit root_path
+
+        page.should have_content("Welcome!")
+        page.should have_content("This is Qwiki, the qwik wiki.")
+
+        current_path.should == root_path
+      end
+
+      it "allows the user to sign in" do
+        visit root_path
+
+        fill_in("user_email",                 :with => user.email)
+        fill_in("user_password",              :with => "password")
+        click_button("Sign in")
+
+        page.should have_content("Signed in successfully.")
+
+        current_path.should == root_path
+      end
+    end
+
+    context "given a user has not logged in" do
+      it "displays the home index with information about qwiki" do
+        visit root_path
+
+        page.should have_content("Sign in")
+
+        current_path.should == new_user_session_path
+      end
+
+      it "allows the user to sign up" do
+        visit root_path
+
+        click_link("Sign up")
+
+        User.count.should == 0
+
+        fill_in("user_email",                 :with => "test_2@example.com")
+        fill_in("user_password",              :with => "password")
+        fill_in("user_password_confirmation", :with => "password")
+        fill_in("user_first_name",            :with => "Johnny")
+        fill_in("user_last_name",             :with => "Signup")
+        click_button("Sign up")
+
+        User.count.should == 1
+
+        page.should have_content("You have signed up successfully.")
+
+        current_path.should == root_path
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,8 @@ Spork.prefork do
   require 'rspec/autorun'
   require 'database_cleaner'
   require 'capybara/rspec'
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
@@ -32,7 +34,7 @@ Spork.prefork do
 
   RSpec.configure do |config|
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = true
+    config.use_transactional_fixtures = false
     config.use_transactional_examples = false
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
