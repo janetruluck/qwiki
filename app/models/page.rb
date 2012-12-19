bdaf9e1d8892f86c8f1af09c7f90c9f57968d7a4
Jason Truluck <jason.truluck@gmail.com>
2012-12-19 01:53:12 -0500

    added tagging to page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,13 @@
 class Page < ActiveRecord::Base
-  attr_accessible :user_id, :content, :title
+  attr_accessible :user_id, :content, :title, :tag_list
 
   validates :title, :content, :presence => true
 
   belongs_to :user
   has_many :histories, :dependent => :destroy
+
+  acts_as_ordered_taggable
+  acts_as_ordered_taggable_on :tags
+
+  default_scope order('created_at DESC')
 end
